871e5178b78e48714e521d40dbb9afb22eb9e5b6
PiotrHCpp <piotr.hoffner.wroc@gmail.com>
2020-08-11 20:42:04 +0200

    primeNumbers: new TestingValues for IsPrimeNumberTests

--- primeNumbers/IsPrimeNumberTests.cpp
+++ primeNumbers/IsPrimeNumberTests.cpp
@@ -18,14 +18,22 @@ INSTANTIATE_TEST_SUITE_P(ParamT1,
                          ::testing::Values(
                                            TestingValues{0, false},
                                            TestingValues{1, false},
+                                           TestingValues{4, false},
+                                           TestingValues{6, false},
+                                           TestingValues{20, false},
+                                           TestingValues{1214, false},
+                                           TestingValues{1215, false},
+                                           TestingValues{1216, false},
+                                           TestingValues{10068, false},
+                                           TestingValues{11577, false},
                                            TestingValues{2, true},
                                            TestingValues{3, true},
-                                           TestingValues{4, false},
                                            TestingValues{5, true},
-                                           TestingValues{6, false},
                                            TestingValues{7, true},
-                                           TestingValues{20, false},
-                                           TestingValues{23, true}
+                                           TestingValues{1213, true},
+                                           TestingValues{1217, true},
+                                           TestingValues{10067, true},
+                                           TestingValues{11003, true}
 ));
 
 TEST_P(IsPrimeNumberTests, checkExpectedForIsPrimeNumber) {
