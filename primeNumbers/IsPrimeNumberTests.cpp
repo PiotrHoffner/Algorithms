5005a7d4dea75f2034455c57d81ba16fca37b182
PiotrHCpp <piotr.hoffner.wroc@gmail.com>
2020-08-11 20:09:20 +0200

    primeNumbers: isPrimeNumber returns true if 23 passed for argument

--- primeNumbers/IsPrimeNumberTests.cpp
+++ primeNumbers/IsPrimeNumberTests.cpp
@@ -28,4 +28,6 @@ TEST(IsPrimeNumberTests, returnTrueIfPassed7) {
 TEST(IsPrimeNumberTests, returnFalseIfPassed20) {
     EXPECT_EQ(false, isPrimeNumber(20));
 }
-
+TEST(IsPrimeNumberTests, returnTrueIfPassed23) {
+    EXPECT_EQ(true, isPrimeNumber(23));
+}
