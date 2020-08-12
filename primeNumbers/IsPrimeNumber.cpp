d23ee4096583ab7386dcdbf33eeae9d1b6bfcb24
PiotrHCpp <piotr.hoffner.wroc@gmail.com>
2020-08-12 09:07:52 +0200

    primeNumbers: removed unnecessary bool isArgumentPrime

--- primeNumbers/IsPrimeNumber.cpp
+++ primeNumbers/IsPrimeNumber.cpp
@@ -1,18 +1,15 @@
 #include "IsPrimeNumber.hpp"
 
-#include <cmath>
 #include <stdexcept>
 
 bool isPrimeNumber(const unsigned argument) {
     if(argument == 0 || argument == 1) {
         return false;
     }
-    bool isArgumentPrime = true;
     for(unsigned divisor = 2; divisor < argument; ++divisor) {
         if(argument % divisor == 0) {
-            isArgumentPrime = false;
-            break;
+            return false;
         }
     }
-    return isArgumentPrime;
+    return true;
 }
