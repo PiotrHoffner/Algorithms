fe6e17164c70e081aed4f40295152b3a85b729bf
PiotrHCpp <piotr.hoffner.wroc@gmail.com>
2020-08-12 08:26:07 +0200

    bool palindrome() function refactor

--- palindrome/Palindrome.cpp
+++ palindrome/Palindrome.cpp
@@ -1,21 +1,8 @@
 #include "Palindrome.hpp"
 
+#include <algorithm>
+
 bool palindrome(const std::string& str) {
-    if(str.size() == 0) {
-        return false;
-    }
-    if(str.size() == 1) {
-        return true;
-    }
-    if(str.size() == 2 && str[0] == str[1]) {
-        return true;
-    }
-    bool firstAndLastCharMatches = str[0] == str[str.size()-1];
-    if(str.size() == 3 && firstAndLastCharMatches) {
-        return true;
-    }
-    if(str.size() == 4 && firstAndLastCharMatches && str[1] == str[str.size()-2]) {
-        return true;
-    }
-    return false;
+    if(str.size() == 0) return false;
+    return std::equal(str.cbegin(), str.cbegin() + str.size() / 2, str.crbegin());
 }
